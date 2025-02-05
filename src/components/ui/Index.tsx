ac67154259f32dc7b83065a5e011b8066cbbe847
sebastiangaleano.dev <sebastiangaleano.dev@gmail.com>
2025-02-05 12:06:47 +0000

    Index component - card an titles changes

--- src/components/ui/Index.tsx
+++ src/components/ui/Index.tsx
@@ -5,10 +5,13 @@ export default function Index() {
         <section className="min-h-screen bg-white p-8">
             <section className="grid gap-8">
                 <section
-                    className="bg-gray-100 p-4 rounded-lg shadow-lg transform transition-all duration-300 hover:scale-105"
+                    className="mt-6 p-4 rounded-lg shadow-lg bg-white border-2 border-yellow-500"
                 >
-                    <section className="h-80 bg-cover bg-center flex items-center justify-center" style={{ paddingBottom: "100%" }, { backgroundImage: "url('https://multer-upload-api.s3.us-east-2.amazonaws.com/images/DigitalCard/LS%2BRing')" }}>
+                    <section 
+                    className="h-80 mb-8 bg-cover bg-center flex items-center justify-center rounded-lg" 
+                    style={{ backgroundImage: "url('https://multer-upload-api.s3.us-east-2.amazonaws.com/images/DigitalCard/LS%2BRing')" }}>
                     </section>
+                    <h2 className="title-animate text-black text-2xl font-bold text-center mb-2">Luciana & Sebastian</h2>
                 </section>
             </section>
             <h1 className="title-animate text-4xl font-bold text-center text-black mt-8">Nos casamos ♥</h1>
