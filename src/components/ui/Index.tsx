9a184217e69dfc485571820932487b46a05e210a
sebastiangaleano.dev <sebastiangaleano.dev@gmail.com>
2025-02-06 13:45:19 +0000

    Index component - background section addeed

--- src/components/ui/Index.tsx
+++ src/components/ui/Index.tsx
@@ -2,9 +2,17 @@ import { Button } from "./Button"
 
 export default function Index() {
     return (
-        <section className="min-h-screen bg-gray-100 p-4">
-            <h1 className="title-animate title-elegant text-6xl font-bold text-center text-gray-600 m-6">Nos casamos ♥</h1>
-            <section className="grid gap-8">
+        <section
+            className="p-4 min-h-screen"
+            style={
+                {
+                    backgroundImage: "url('https://multer-upload-api.s3.us-east-2.amazonaws.com/images/DigitalCard/Font4')",
+                    backgroundAttachment: "fixed",
+                    backgroundSize: "contain",
+                }}
+        >  <h1 className="title-animate title-elegant text-6xl font-bold text-center text-black mt-6">Nos casamos ♥</h1>
+
+                      <section className="grid gap-8 mt-6">
                 <section
                     className="p-4 rounded-lg shadow-lg bg-white border-2 border-yellow-500"
                 >
@@ -12,10 +20,10 @@ export default function Index() {
                         className="h-80 mb-8 bg-cover bg-center flex items-center justify-center rounded-lg border-2 border-yellow-200"
                         style={{ backgroundImage: "url('https://multer-upload-api.s3.us-east-2.amazonaws.com/images/DigitalCard/LS%2BRing')" }}>
                     </section>
-                    <h2 className="title-animate title-elegant text-black text-4xl font-bold text-center mb-2">Luciana & Sebastian</h2>
+                    <h2 className="title-animate title-elegant text-black text-4xl font-bold text-center mb-2">Luciana y Sebastian</h2>
                 </section>
             </section>
-                <h3 className="text-black text-2xl font-bold title-elegant text-center mt-4">Ingresar</h3>
+            <h3 className="text-black text-2xl font-bold title-elegant text-center mt-4">Ingresar</h3>
             <section className="flex justify-center items-center flex-col">
                 <Button href="/Home" />
             </section>
