192a3113462cfadfb3ff2651a72b0edee3a44bbb
SebastianGaleano20 <sebascarp9@gmail.com>
2025-10-09 14:08:36 -0300

    feat ( AboutUs ) - change bg

--- src/components/ui/sections/AboutUs.tsx
+++ src/components/ui/sections/AboutUs.tsx
@@ -1,19 +1,29 @@
-import animation from '@/src/styles/Animation.module.css'
+import animation from "@/src/styles/Animation.module.css";
+import Image from "next/image";
 export default function AboutUs() {
-    return (
-        <section>
-            <section
-                className="h-96 bg-cover bg-center bg-no-repeat relative flex flex-col gap-4 items-center justify-center"
-                style={{
-                    backgroundImage: "url('https://multer-upload-api.s3.us-east-2.amazonaws.com/images/DigitalCard/Luz')",
-                }}
-            >
-                <section className="absolute inset-0 bg-gray-600 bg-opacity-50"></section>
-                <h2 className={`${animation.title} text-white font-bolt text-3xl`}>Nos casamos</h2>
-                <p className={`${animation.scaleUp} block elegant p-2 font-bold text-white text-center text-5xl z-10`}>Luciana <br />y<br /> Sebastian</p>
-                {/* <h2 className={`${animation.title} text-white font-bolt text-3xl`}>20 DICIEMBRE 2025</h2> */}
-            </section>
-            <h2 className="font-bold text-3xl text-center elegant m-4 text-gray-800">Te invitamos a celebrar con nosotros</h2>
+  return (
+    <section>
+      <section>
+        <section className="relative w-fit">
+          <Image
+            src="/assets/5.png"
+            alt="image"
+            width={600}
+            height={600}
+            className="object-cover"
+          />
+          <p
+            className={`${animation.scaleUp} elegant p-2 font-bold text-white text-center mb-10 text-6xl z-10 absolute inset-0 flex items-end justify-center`}
+          >
+            Luciana <br />y<br /> Sebastian
+          </p>
         </section>
-    )
-} 
+
+        {/* <h2 className={`${animation.title} text-white font-bolt text-3xl`}>20 DICIEMBRE 2025</h2> */}
+      </section>
+      <h2 className="font-bold text-3xl text-center elegant m-4 text-gray-800">
+        Te invitamos a celebrar con nosotros
+      </h2>
+    </section>
+  );
+}
