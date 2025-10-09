f464ebcc41d10ec7cf262e2a46886870d9982b75
SebastianGaleano20 <sebascarp9@gmail.com>
2025-10-09 14:07:49 -0300

    fix ( ButtonIndex ) - url logo

--- src/components/ui/ButtonIndex.tsx
+++ src/components/ui/ButtonIndex.tsx
@@ -1,32 +1,35 @@
-'use client'
+"use client";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import styles from "../../styles/Button.module.css";
 import Image from "next/image";
 
 interface ButtonProps {
-    href?: string;
+  href?: string;
 }
 
 export function Button({ href }: ButtonProps) {
-    const [isExpand, setIsExpand] = useState(false);
-    const router = useRouter();
+  const [isExpand, setIsExpand] = useState(false);
+  const router = useRouter();
 
-    const handleClick = () => {
-        setIsExpand(true);
+  const handleClick = () => {
+    setIsExpand(true);
 
-        // Esperar a que la animación termine antes de redirigir
-        setTimeout(() => {
-            setIsExpand(false);
-            if (href) {
-                router.push(href); // Redirecciona después de 800 ms
-            }
-        }, 600); // Duración de la animación
-    };
+    // Esperar a que la animación termine antes de redirigir
+    setTimeout(() => {
+      setIsExpand(false);
+      if (href) {
+        router.push(href); // Redirecciona después de 800 ms
+      }
+    }, 600); // Duración de la animación
+  };
 
-    return (
-        <button className={`${isExpand ? styles.expand : ""}`} onClick={handleClick}>
-            <Image src="/logo.png" width={130} height={130} alt="logo" />
-        </button>
-    );
+  return (
+    <button
+      className={`${isExpand ? styles.expand : ""}`}
+      onClick={handleClick}
+    >
+      <Image src="/assets/logo.png" width={130} height={130} alt="logo" />
+    </button>
+  );
 }
