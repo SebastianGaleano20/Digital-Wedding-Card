42d5eb035b0446cbe5b1404631947ac94f08c6b3
SebastianGaleano20 <sebascarp9@gmail.com>
2025-10-22 15:53:44 -0300

    refactor(AboutUs): Delete info - change image

--- src/components/ui/sections/AboutUs.tsx
+++ src/components/ui/sections/AboutUs.tsx
@@ -1,4 +1,3 @@
-import animation from "@/src/styles/Animation.module.css";
 import Image from "next/image";
 export default function AboutUs() {
   return (
@@ -6,24 +5,14 @@ export default function AboutUs() {
       <section>
         <section className="relative w-fit">
           <Image
-            src="/assets/5.png"
+            src="/assets/Index.png"
             alt="image"
             width={600}
             height={600}
             className="object-cover"
           />
-          <p
-            className={`${animation.scaleUp} elegant p-2 font-bold text-white text-center mb-10 text-6xl z-10 absolute inset-0 flex items-end justify-center`}
-          >
-            Luciana <br />y<br /> Sebastian
-          </p>
         </section>
-
-        {/* <h2 className={`${animation.title} text-white font-bolt text-3xl`}>20 DICIEMBRE 2025</h2> */}
       </section>
-      <h2 className="font-bold text-3xl text-center elegant m-4 text-gray-800">
-        Te invitamos a celebrar con nosotros
-      </h2>
     </section>
   );
 }
